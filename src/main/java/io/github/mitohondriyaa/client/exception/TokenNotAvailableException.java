2f65831c4972f5134cb94445abd657d62f8aac45
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-02 19:22:42 +0300

    Add token not available exception

---
+++ src/main/java/io/github/mitohondriyaa/client/exception/TokenNotAvailableException.java
@@ -0,0 +1,7 @@
+package io.github.mitohondriyaa.client.exception;
+
+public class TokenNotAvailableException extends RuntimeException {
+    public TokenNotAvailableException(String message) {
+        super(message);
+    }
+}
