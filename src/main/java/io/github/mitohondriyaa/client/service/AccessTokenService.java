e344b0044dac04479850d8e8836357e49c0057d6
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-02 19:19:18 +0300

    Add access token service and fix mistake in .properties file

---
+++ src/main/java/io/github/mitohondriyaa/client/service/AccessTokenService.java
@@ -0,0 +1,30 @@
+package io.github.mitohondriyaa.client.service;
+
+import io.github.mitohondriyaa.client.model.AccessToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccessTokenService {
+    private final OAuth2AuthorizedClientManager authorizedClientManager;
+
+    public AccessToken getAccessToken() {
+        OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
+            .withClientRegistrationId("security-client")
+            .build();
+        OAuth2AuthorizedClient authorizedClient = authorizedClientManager
+            .authorize(authorizeRequest);
+
+        if (authorizedClient == null) {
+            throw new RuntimeException();
+        }
+
+        return new AccessToken(
+            authorizedClient.getAccessToken().getTokenValue()
+        );
+    }
+}
