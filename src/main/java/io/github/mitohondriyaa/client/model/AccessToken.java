8291a597b891e43686ff00b453ba6531a21e032e
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-02 19:03:08 +0300

    Add access token model

---
+++ src/main/java/io/github/mitohondriyaa/client/model/AccessToken.java
@@ -0,0 +1,14 @@
+package io.github.mitohondriyaa.client.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AccessToken {
+    private String accessToken;
+}
