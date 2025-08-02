0cb822b3f6b193381e62df58d56cc4984d14743d
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-02 20:53:02 +0300

    Add unit test

--- src/test/java/io/github/mitohondriyaa/client/SecurityClientApplicationTests.java
+++ src/test/java/io/github/mitohondriyaa/client/SecurityClientApplicationTests.java
@@ -1,13 +1,35 @@
 package io.github.mitohondriyaa.client;
 
+import io.github.mitohondriyaa.client.model.AccessToken;
+import io.github.mitohondriyaa.client.service.AccessTokenService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@AutoConfigureMockMvc
+@RequiredArgsConstructor
 class SecurityClientApplicationTests {
+	private final MockMvc mockMvc;
+	@MockitoBean
+	private AccessTokenService accessTokenService;
 
 	@Test
-	void contextLoads() {
-	}
+	void shouldReturnAccessToken() throws Exception {
+		when(accessTokenService.getAccessToken())
+			.thenReturn(new AccessToken("mock-token"));
 
+		mockMvc.perform(get("/api/access-token"))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("accessToken").value("mock-token"));
+	}
 }
