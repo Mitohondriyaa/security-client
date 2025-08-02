7651511b0b330a0fa7aeaab2e1e7ed1825f81c11
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-03 01:18:29 +0300

    Change principal in service

--- src/main/java/io/github/mitohondriyaa/client/service/AccessTokenService.java
+++ src/main/java/io/github/mitohondriyaa/client/service/AccessTokenService.java
@@ -3,6 +3,8 @@ package io.github.mitohondriyaa.client.service;
 import io.github.mitohondriyaa.client.exception.TokenNotAvailableException;
 import io.github.mitohondriyaa.client.model.AccessToken;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
@@ -12,11 +14,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class AccessTokenService {
     private final OAuth2AuthorizedClientManager authorizedClientManager;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
 
     public AccessToken getAccessToken() {
         OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest
             .withClientRegistrationId("security-client")
-            .principal("security-client")
+            .principal(new UsernamePasswordAuthenticationToken(username, password))
             .build();
         OAuth2AuthorizedClient authorizedClient = authorizedClientManager
             .authorize(authorizeRequest);
