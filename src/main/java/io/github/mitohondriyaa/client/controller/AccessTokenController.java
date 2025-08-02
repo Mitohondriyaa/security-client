4c2ed4fa073a0144c795ef455f44075a78114ad7
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-02 20:08:19 +0300

    Add security config

--- src/main/java/io/github/mitohondriyaa/client/controller/AccessTokenController.java
+++ src/main/java/io/github/mitohondriyaa/client/controller/AccessTokenController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/access-token")
+@RequestMapping("/api/token")
 @RequiredArgsConstructor
 public class AccessTokenController {
     private final AccessTokenService accessTokenService;
