c18be26eafdf83f5c4a3a12091b41a3fc22c93d4
Mitohondriyaa <mitohondriyaa@gmail.com>
2025-08-03 10:46:31 +0300

    Add keycloak client

---
+++ src/main/java/io/github/mitohondriyaa/client/client/KeycloakClient.java
@@ -0,0 +1,16 @@
+package io.github.mitohondriyaa.client.client;
+
+import io.github.mitohondriyaa.client.model.AccessToken;
+import org.springframework.http.MediaType;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.service.annotation.PostExchange;
+
+public interface KeycloakClient {
+    @PostExchange(
+        value = "http://localhost:8088/realms/microservices-realm" +
+            "/protocol/openid-connect/token",
+        contentType = MediaType.APPLICATION_FORM_URLENCODED_VALUE
+    )
+    AccessToken getAccessToken(@RequestBody MultiValueMap<String, String> body);
+}
